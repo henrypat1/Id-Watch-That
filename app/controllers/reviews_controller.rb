e43688565e544f32f7dbef046b30184c9c42b405
Henry Patterson <henrycpatterson92@gmail.com>
2021-08-26 15:56:24 -0400

    migrate

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,28 +24,4 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  def update
-    if @review.update(review_params)
-      render json: @review
-    else
-      render json: @review.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /reviews/1
-  def destroy
-    @review.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def review_params
-      params.require(:review).permit(:review, :user_id, :movie_id)
-    end
 end
