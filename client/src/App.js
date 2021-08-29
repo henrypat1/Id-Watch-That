8f701ff7479ddbe71205428a500964ebd3da2f01
Henry Patterson <henrycpatterson92@gmail.com>
2021-08-29 17:39:14 -0400

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import './App.css';
+
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleSignIn = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleSignUp = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout currentUser={currentUser}>
+        <Switch>
+          <Route path='/SignIn'>
+            <Login handleSignIn={handleLogin} />
+          </Route>
+          <Route path='/SignUp'>
+            <Register handleRegister={handleRegister} />
+          </Route>
+          <Route path='/'>
+            <MainContainer currentUser={currentUser} />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
